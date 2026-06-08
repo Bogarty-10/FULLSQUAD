be3f531b8fb5ec1716f2e750fa624323c1d62424
Bogarty <modosuperandy@gmail.com>
2026-06-08 19:41:37 +0200

    Validacion de confirmar contraseña de register y testeo correcto.

--- app/src/main/java/com/example/fullsquad/RegisterActivity.java
+++ app/src/main/java/com/example/fullsquad/RegisterActivity.java
@@ -27,7 +27,11 @@ public class RegisterActivity extends AppCompatActivity {
 
         EditText etUsuario = findViewById(R.id.editTUsuario);
 
+        EditText etCorreo = findViewById(R.id.editTCorreo);
+
         EditText etPassword = findViewById(R.id.editTPass);
+
+        EditText etConfirmPass = findViewById(R.id.editTConfirmPass);
 
         btnRegistrarse.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,12 +39,42 @@ public class RegisterActivity extends AppCompatActivity {
                 String usuario = etUsuario.getText().toString().trim();
                 String password = etPassword.getText().toString().trim();
 
+                String confirmPass = etConfirmPass.getText().toString().trim();
+
+                String correo = etCorreo.getText().toString().trim();
+
                 if (usuario.isEmpty() || password.isEmpty()) {
                     Toast.makeText(RegisterActivity.this, "Rellena todos los campos", Toast.LENGTH_SHORT).show();
                     return;
                 }
 
                 DBHelper dbHelper = new DBHelper(RegisterActivity.this);
+
+                // Validar campos vacíos
+                if (usuario.isEmpty() || correo.isEmpty()||
+                        password.isEmpty() ||
+                        confirmPass.isEmpty()) {
+
+                    Toast.makeText(
+                            RegisterActivity.this,
+                            "Rellena todos los campos",
+                            Toast.LENGTH_SHORT
+                    ).show();
+
+                    return;
+                }
+
+// Validar contraseñas
+                if (!password.equals(confirmPass)) {
+
+                    Toast.makeText(
+                            RegisterActivity.this,
+                            "Las contraseñas no coinciden",
+                            Toast.LENGTH_SHORT
+                    ).show();
+
+                    return;
+                }
 
                 boolean insertado = dbHelper.insertarUsuario(usuario, password);
 
